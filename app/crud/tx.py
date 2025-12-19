b1d4646666aa4eef734d6e2df9c3353186330a42
Slava <steschenko.vyacheslav@gmail.com>
2025-12-19 16:30:32 +0100

    Tests

--- app/crud/tx.py
+++ app/crud/tx.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, delete
+from sqlalchemy import select
 from app.models import Transactions
 
 async def create_tx(db: AsyncSession, **data) -> Transactions:
--- app/tests/test_tx.py
+++ app/tests/test_tx.py
@@ -16,33 +16,32 @@ async def create_tx(auth_client):
 
 @pytest.mark.asyncio
 async def test_create_tx(auth_client):
-    create_tx(auth_client)
+    await create_tx(auth_client)
 
 @pytest.mark.asyncio
 async def test_get_all_txs(auth_client):
-    await create_tx(auth_client)
+    created = await create_tx(auth_client)
+
     res = await auth_client.get("/transactions")
+    assert res.status_code == 200
+
     data = res.json()
-    assert data["transactions"][0]["type"] == "expense"
-    assert data["transactions"][0]["amount"] == -25
-    assert data["transactions"][0]["category"] == "grocery"
-    assert data["transactions"][0]["place"] == "DELHAIZE"
+    txs = data["transactions"]
+    assert isinstance(txs, list)
+
+    fetched = next((tx for tx in txs if tx["id"] == created["id"]), None)
+    assert fetched is not None
+
+    assert fetched["type"] == "expense"
+    assert fetched["amount"] == -25
+    assert fetched["category"] == "grocery"
+    assert fetched["place"] == "DELHAIZE"
 
 @pytest.mark.asyncio
 async def test_update_tx(auth_client):
-    res = await auth_client.post("/transactions", json={
-        "type": "expense",
-        "amount": 25,
-        "category": "grocery",
-        "place": "DELHAIZE",
-    })
-    assert res.status_code == 201
-    data = res.json()
-    assert data["message"] == "Transaction created"
-    tx = data["transaction"]
+    tx = await create_tx(auth_client)
     assert tx["amount"] == -25
-    tx = data["transaction"]
-    tx_id = int(tx["id"])
+    tx_id = tx["id"]
     
     res = await auth_client.put(f"/transactions/{tx_id}", json={
         "type": "expense",
@@ -61,4 +60,14 @@ async def test_update_tx(auth_client):
 @pytest.mark.asyncio
 async def test_delete_tx(auth_client):
     tx = await create_tx(auth_client)
-    print(tx)
+    tx_id = tx["id"]
+    res = await auth_client.get("/transactions")
+    data = res.json()
+    before = data["transactions"]
+    res = await auth_client.delete(f"/transactions/{tx_id}")
+    assert res.status_code == 202
+    res = await auth_client.get("/transactions")
+    data = res.json()
+    after = data["transactions"]
+    assert len(before) == 1
+    assert len(after) == 0
